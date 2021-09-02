3a27efad65d9bf80e28aa5fb92acd6392b5524f8
hanhnhitran <hanhnhi0705@gmail.com>
2021-09-02 21:47:46 +0700

    js1

---
+++ lesson5/index.js
@@ -0,0 +1,12 @@
+
+var person = {
+    firstName: 'Hanh',
+    lastName: 'Nhi',
+}
+
+function getname(firstName, lastName) {
+    console `${firstName} ${lastName}`;
+}
+
+let s1 = 'hanhnhi';
+console.log( 'do dai string la : ' +  s1.length());
