34ed89e3af1e9aaf535063b0d0424cae5405d97f
hanhnhitran <hanhnhi0705@gmail.com>
2021-10-24 16:55:41 +0700

    final

---
+++ index.js
@@ -0,0 +1,22 @@
+// var items = [
+//     [2, 1, 1],
+//     [1, 2, 1],
+//     [1, 1, 2]
+//   ];
+//   console.log(items[0][0]);     
+//   console.log(items[0][1]); 
+//   console.log(items[1][0]); 
+//   console.log(items);
+
+
+var testArray1 = [2, 1, 1]
+var testArray2 = [1, 2, 1]
+var testArray3 = [1, 1, 2]
+var testArray4 = {"test":123}
+var index = 0;
+
+myDoubleArray[index++] = testArray1;
+myDoubleArray[index++] = testArray2;
+myDoubleArray[index++] = testArray3;
+
+console.log(myDoubleArray[0],myDoubleArray[1][3], myDoubleArray[2]['test'],) 
---
+++ python/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "this is the home page"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
+
