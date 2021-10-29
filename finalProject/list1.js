d0c49a633d9e20945d0f7f49c0684815bdc17f3e
hanhnhitran <hanhnhi0705@gmail.com>
2021-10-29 14:53:05 +0700

    final

--- finalProject/list1.js
+++ finalProject/list1.js
@@ -2,34 +2,64 @@ var container = document.querySelector(".movie-container");
 
 var data = [
   {
-    img: "https://kobis.or.kr/common/mast/movie/2018/10/3a4267d7bce04106b996ad93e65a57d1.jpg",
+    img: "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_FMjpg_UX1000_.jpg",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "High Society",
+    name: "Star war",
   },
   {
-    img: "https://newsmd1fr.keeng.net/tiin/archive/images/20201030/115331_61zajvmda1l_sl1101_.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/vi/2/2d/Avengers_Endgame_bia_teaser.jpg",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "The King",
+    name: "End game",
   },
   {
-    img: "https://upload.wikimedia.org/wikipedia/en/2/26/Mine_TV_series.jpg",
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/stb_teaser_1-sht_v4_lg.jpg  ",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "Mine",
+    name: "Shang chi",
   },
   {
-    img: "https://dep.com.vn/wp-content/uploads/2021/06/banner-truong-luat-law-school-2021.jpg",
+    img: "https://lumiere-a.akamaihd.net/v1/images/p_avengersinfinitywar_19871_cb171514.jpeg?region=0,0,540,810&width=480",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "Law School",
+    name: "Infiniti war",
   },
   {
-    img: "https://cdn.shopify.com/s/files/1/1374/2665/products/800x0.png?v=1616656958",
+    img: "https://lumiere-a.akamaihd.net/v1/images/p_blackpanther_19754_4ac13f07.jpeg?region=0,0,540,810&width=480",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "Penhouse",
+    name: "Black panther",
   },
   {
-    img: "https://upload.wikimedia.org/wikipedia/vi/archive/1/10/20210302064452%21What%27s_Wrong_with_Secretary_Kim.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg",
     link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
-    name: "What's wrong with kim secretary",
+    name: "Advengers",
+  },
+  {
+    img: "https://i.pinimg.com/736x/13/db/2d/13db2dfef0d7fe0b0c205d8dd3c83b02.jpg",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Infiniti war",
+  },
+  {
+    img: "https://cdn.tgdd.vn//GameApp/1338713//5-800x1186.jpg",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Thor",
+  },
+  {
+    img: "https://media.senscritique.com/media/000018355538/source_big/Captain_Marvel.jpg",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Captain Marvels",
+  },
+  {
+    img: "https://lumiere-a.akamaihd.net/v1/images/image_ae30e307.jpeg?region=0,0,540,810&width=480",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Antman the wasp",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/en/7/74/Shang-Chi_and_the_Legend_of_the_Ten_Rings_poster.jpeg",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Shang chi",
+  },
+  {
+    img: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/guardiansofthegalaxy_lob_crd_03.jpg",
+    link: "https://www.netflix.com/title/81026705?trackId=13752289&trackIdJaw=13752289&trackIdEpisode=13752289&trackIdTrailer=13752289&dpRightClick=1",
+    name: "Guardians of the galaxy",
   },
 ];
 
