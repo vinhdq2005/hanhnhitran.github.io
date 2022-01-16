1e97f3e48bbeffd35f59a0f3a6a3153059f95bc6
hanhnhitran <hanhnhi0705@gmail.com>
2022-01-16 14:15:20 +0700

    weather

--- JSA/lesson7/main.js
+++ JSA/lesson7/main.js
@@ -1,16 +1,11 @@
-let formin = document.getElementById("input");
-
-formin.onsubmit = function (e) {
-  e.preventDefault();
-  let key = formin.cityName.value;
-  Getdata(key);
-};
-
-async function Getdata(city) {
-  let drawdata = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7674da634845e7c2d8c53ab0a48b8e29`
+Weather.getCurrent("Ho Chi Minh City", function(current) {
+  console.log(
+    ["currently:",current.temperature(),"and",current.conditions()].join(" ")
   );
-  let data = await drawdata.json();
-  console.log(data.main);
-  console.log(data.weather);
-}
+});
+ 
+Weather.getForecast("Ho Chi Minh City", function(forecast) {
+  console.log("forecast high: " + forecast.high());
+  console.log("forecast low: " + forecast.low());
+});
+ 
