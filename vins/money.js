5c50ef5e8f0d00ff6a5d9e7fd1bf8c1c46431125
hanhnhitran <hanhnhi0705@gmail.com>
2022-02-16 07:13:08 +0700

    project

--- vins/money.js
+++ vins/money.js
@@ -1,1 +1,9 @@
-const dropList = document.querySelectorAll("")
+const dropList = document.querySelectorAll(".drop-list select");
+
+for (let i = 0; i < dropList.length; i++) {
+    for(currency_code in country_code){ 
+        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+}
+
