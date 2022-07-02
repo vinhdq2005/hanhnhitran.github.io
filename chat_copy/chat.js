1d4bf36cdb6e6a488f2f5e4f728babb9879195d4
hanhnhitran <hanhnhi0705@gmail.com>
2022-07-02 10:57:30 +0700

    chat

--- chat_copy/chat.js
+++ chat_copy/chat.js
@@ -3,18 +3,27 @@ document.querySelector("#sw_mode").addEventListener("click", function () {
   document.body.classList.toggle("dark_mode");
 });
 
+
+var email_ = ""
+var currentid_ = ""
+var img_ = ""
+
 // check xem sự thay đổi của user đã được lưu trong firebase hay chưa
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     var uid = user.uid;
-
+    console.log(user.email);
+    email_ = user.email
+    img_ = user.photoURL
     loadchat(user.email);
     renderCurrentUser(user.photoURL, user.displayName)
     console.log(user);
     // ...
   } else {
-    alert("Bạn chưa đăng nhập");
-    open("./signin/index.html", "_self");
+    sweetAlert("warning", "bạn cần đăng nhập");
+    setTimeout(() => {
+      open("./signin/index.html", "_self");
+    }, 3000);
   }
 });
 
@@ -30,12 +39,44 @@ let signOut = () => {
     });
 };
 
+let clock = () => {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let d = date.getDay();
+
+  if (m < 10) {
+    m = "0" + m;
+  }
+  if (h < 10) {
+    h = "0" + h;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  }
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d];
+
+  return ` ${h}:${m}:${s}     ${n}`;
+};
+
+
 let loadchat = async (email) => {
   let result = await firebase
     .firestore()
     .collection("chat")
     .where("users", "array-contains", email)
     .get();
+  console.log(result);
 
   let data = getDataFromDocs(result.docs);
   console.log(data);
@@ -58,11 +99,30 @@ let getDataFromDocs = (docs) => {
   return result;
 };
 
+
+let sweetAlert = (icon, message) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: icon,
+    title: message,
+  });
+};
+
 let renderChat = (data, ownerEmail) => {
   let chat = data.chat;
+  currentid_ = data.id
   let dom = document.querySelector(".chat");
-  document.querySelector("#currentUserImg").src = data.avatar;
-  document.querySelector("#currentUserName").innerHTML = data.name;
   dom.innerHTML = "";
 
   for (let i = 0; i < chat.length; i++) {
@@ -105,13 +165,50 @@ let renderUserList = (data) => {
 
     dom.innerHTML += html;
   }
+  for (let i = 0; i < data.length; i++) {
+    let user = document.querySelector(`#c${data[i].id}`);
+    user.addEventListener("click", () => {
+      renderChat(data[i], email);
+    });
+  }
 };
-
 
 let renderCurrentUser = (photo, userName) => {
   document.querySelector("#userImg").src = photo
   document.querySelector("#userName").innerHTML = userName
 }
+
+setInterval(() => {
+  document.querySelector("#realTime").innerHTML = clock();
+}, 1000);
+
+let formChat = document.querySelector("#chat_input_form");
+formChat.onsubmit = (e) => {
+  e.preventDefault();
+
+  let content = formChat.chat.value.trim();
+
+  updateNewMessage(content, email_, img_, clock(), currentid_)
+};
+
+let updateNewMessage = async (content, email, img, time, currentID) => {
+  if (currentID) {
+    let conversationId = currentID;
+    let message = {
+      content: content,
+      time: time,
+      img: img,
+      email: email,
+    };
+    await firebase
+      .firestore()
+      .collection("chat")
+      .doc(conversationId)
+      .update({
+        chat: firebase.firestore.FieldValue.arrayUnion(message),
+      });
+  }
+};
 
 // let loadConversation1 = async (email)=>{
 //     var currentEmail = email.trim()
